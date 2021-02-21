d48c5b4f65ed8a74e2dc4d47b0f4d99a26423f6a
Fiona Au <fionaonafi@gmail.com>
2021-02-20 21:40:06 -0500

    first commit

---
+++ State.java
@@ -0,0 +1,15 @@
+public class State {
+    /* a state has a board, has an arraylist of child states. Implement the following methods?
+    RESULT,  applicability function???*/
+    //shouldnt need set of states because that is way too much to include
+    
+    // Result(State s, Action a) { //if applicable returns true, then it would place the tile on the board
+
+    // }
+
+    // Applicable(Action a) { //test if a move is legal
+    //     //is there already a piece there? -> if so, cannot place there
+    //     //is it going to be next to an enemy piece in a way that traps them?
+    // }
+
+}
